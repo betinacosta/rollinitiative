f74adc1daebee31d4220ec9e598a0a77e7b519a9
Betina Costa <becosta@thoughtworks.com>
2019-04-03 10:26:54 -0300

    initial commit

---
+++ build/precache-manifest.8cadf08dec403b7571213d0d7713890d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "902f04bcdb9e8377778b",
+    "url": "/static/js/main.eedac8a8.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "902f04bcdb9e8377778b",
+    "url": "/static/css/main.db06bcd1.chunk.css"
+  },
+  {
+    "revision": "c4a67d2a98dea966b74b779911d16697",
+    "url": "/index.html"
+  }
+];
